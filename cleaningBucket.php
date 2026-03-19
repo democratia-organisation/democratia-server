73afbbef9fd1281bd80f007c04e48a1f68617d93
Zyko <modaherry@gmail.com>
2026-03-19 10:43:22 +0100

    refacto : correction

--- cleaningBucket.php
+++ cleaningBucket.php
@@ -3,8 +3,8 @@
 $tempsVerifUsage = 60*5;
 $tempNettoyage = 3600;
 $w1 = new EvTimer($tempNettoyage,0,function (){
-    Bucket::NettoyerBucket();
-    
+    $verification = Bucket::NettoyerBucket();
+    if (!$verification) throw new Exception("Error Processing Request", CodeDeRetourApi::InternalServerError->value);
 });
 $w2 = new EvTimer($tempsVerifUsage,0,function (){
     $usage = Bucket::getGlobalUsage();
