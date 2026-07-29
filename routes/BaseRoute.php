0334a26a831684f6fbc83cd7eb95238594de2f40
Zyko <modaherry@gmail.com>
2026-07-29 19:55:07 +0000

    fix: correction mineru

--- routes/BaseRoute.php
+++ routes/BaseRoute.php
@@ -2,15 +2,11 @@
 
 namespace Koyok\democratia\routes;
 
-use League\Route\RouteGroup;
-
 final class BaseRoute implements RouterInterface
 {
     public static function Register(): void
     {
         [$router, $_] = Router::GetInstace();
-        $router->group('/', function (RouteGroup $route) {
-            $route->get('/dashboard', []);
-        });
+        $router->get('/dashboard', []);
     }
 }
